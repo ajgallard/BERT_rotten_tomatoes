b328737454ff059321953a8d37801b00e9364d74
Adrian Gallard <ajgallard@lavabit.com>
2021-11-30 11:10:33 -0800

    Create ExtModel.py
    Model Modification with extended classifier section

---
+++ pytorch_scripts/ExtModel.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+from transformers import DistilBertForSequenceClassification as DBSC
+import pytorch_lightning as pl
+from pytorch_lightning import callbacks
+import torchmetrics
+from torch.optim import AdamW
+
+
+"""
+Callbacks
+"""
+early_stopping = callbacks.EarlyStopping(monitor="Val Accuracy", patience=5, verbose=False, mode="max")
+checkpoints = callbacks.ModelCheckpoint(monitor='Val Accuracy', save_top_k=1, filename='best_ckpt-epoch{epoch:02d}')
+callbacks = [early_stopping, checkpoints]
+
+"""
+DistilBERT Model + Lightning
+"""
+class DExt(pl.LightningModule):
+    def __init__(self, params):
+        super(DExt, self).__init__()
+        """Setting Up Hyperparameters from YAML file"""
+        self.params1 = params[1]['model']
+        self.model_name = self.params1["model_name"]
+        self.num_labels = self.params1["num_labels"]
+
+        self.params2 = params[2]['classifier']
+        self.learning_rate = self.params2['learning_rate']
+
+        """Instantiating Model"""
+        model = DBSC.from_pretrained(self.model_name,
+                                     num_labels=self.num_labels)
+        self.model = model
+
+        self.model.classifier = nn.Sequential(
+            nn.Linear(768, 512), nn.BatchNorm1d(512), nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, 256), nn.BatchNorm1d(512), nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256, self.num_labels))
+
+    """
+    LIGHTNING CLASSIFIER SECTION
+    """
+    def configure_optimizers(self):
+        print("Configuring Optimizer...")
+        optimizer = AdamW(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+
+        input_ids = batch['ids']
+        attention_mask = batch['mask']
+        labels = batch['labels']
+
+        outputs = self.model(input_ids=input_ids,
+                             attention_mask=attention_mask)
+
+        # loss
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(outputs.logits, labels)
+
+        # accuracy
+        preds = []
+        for i in range(len(outputs.logits)):
+            label_result = outputs.logits[i]
+            if label_result[0] > label_result[1]:
+                preds.append(0)
+            else:
+                preds.append(1)
+
+        preds = torch.tensor(preds)
+        metric = torchmetrics.Accuracy()
+        acc = metric(preds.cpu(), labels.cpu())
+        self.log("Train Loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("Train Accuracy", acc, prog_bar=True, on_step=False, on_epoch=True)
+        return dict(loss=loss, train_acc=acc)
+
+    def validation_step(self, batch, batch_idx):
+
+        input_ids = batch['ids']
+        attention_mask = batch['mask']
+        labels = batch['labels']
+
+        outputs = self.model(input_ids=input_ids,
+                             attention_mask=attention_mask)
+
+        # loss
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(outputs.logits, labels)
+
+        # accuracy
+        preds = []
+        for i in range(len(outputs.logits)):
+            label_result = outputs.logits[i]
+            if label_result[0] > label_result[1]:
+                preds.append(0)
+            else:
+                preds.append(1)
+
+        preds = torch.tensor(preds)
+        metric = torchmetrics.Accuracy()
+        acc = metric(preds.cpu(), labels.cpu())
+        self.log("Val Loss", loss, prog_bar=True, on_epoch=True)
+        self.log("Val Accuracy", acc, prog_bar=True, on_epoch=True)
+        return dict(loss=loss, val_acc=acc)
+
+    def test_step(self, batch, batch_idx):
+
+        input_ids = batch['ids']
+        attention_mask = batch['mask']
+        labels = batch['labels']
+
+        outputs = self.model(input_ids=input_ids,
+                             attention_mask=attention_mask)
+        preds = []
+        for i in range(len(outputs.logits)):
+            label_result = outputs.logits[i]
+            if label_result[0] > label_result[1]:
+                preds.append(0)
+            else:
+                preds.append(1)
+
+        preds = torch.tensor(preds)
+        return [preds.cpu(), labels.cpu()]  # return predictions & labels for metrics utils
+    
+    def predict_step(self, batch, batch_idx):
+
+        input_ids = batch['ids']
+        attention_mask = batch['mask']
+        labels = batch['labels']
+        
+        outputs = self.model(input_ids=input_ids,
+                             attention_mask=attention_mask)
+        preds = []
+        for i in range(len(outputs.logits)):
+            label_result = outputs.logits[i]
+            if label_result[0] > label_result[1]:
+                preds.append(0)
+            else:
+                preds.append(1)
+
+        preds = torch.tensor(preds)
+        return [preds.cpu(), labels.cpu()]  # return predictions & labels for metrics utils
